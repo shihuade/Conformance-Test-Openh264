ebd71367c75214178ea2c7de849174effeafa289
shihuade <shihuadesysu@gmail.com>
2014-08-26 18:06:31 +0800

    add codec checkout feature

---
+++ run_CheckoutCiscoOpenh264Codec.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+runCheckParameter()
+{
+
+	if [ ! -d ${SourceFolder} ]
+	then
+		echo ""
+		echo -e "\033[31m  Source folder ${SourceFolder} does not exist, please double check!  \033[0m"	
+		echo ""
+		exit 1
+	fi
+	
+	return 0
+}
+
+
+runMain()
+{
+
+	if [ ! $# -eq 2 ]
+	then
+		echo ""
+		echo -e "\033[31m  usage:  ./run_CheckoutCiscoOpenh264Codec.sh  \${GitRepositoryAddr} \${SourceFolder}  \033[0m"
+		echo ""
+		return 1
+	fi
+
+	GitRepositoryAddr=$1
+	SourceFolder=$2
+	
+	runCheckParameter	
+	git clone ${GitRepositoryAddr}  ${SourceFolder}
+	
+	return 0
+}
+GitRepositoryAddr=$1
+SourceFolder=$2
+runMain  ${GitRepositoryAddr} ${SourceFolder}
+
