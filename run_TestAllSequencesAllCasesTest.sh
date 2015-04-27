9923729117b62295d3040d0c5b33aa93491e02a4
huade <huashi@cisco.com>
2015-04-27 09:15:42 +0800

    removed duplicate script files

--- run_TestAllSequencesAllCasesTest.sh
+++ run_TestAllSequencesAllCasesTest.sh
@@ -63,28 +63,61 @@ runGetTestYUVList()
 	
 	aTestYUVList=(${TestSet0}  ${TestSet1}  ${TestSet2}  ${TestSet3}  ${TestSet4}  ${TestSet5}  ${TestSet6}  ${TestSet7} ${TestSet8})
 }
+runInitialSGEJobInfoFile()
+{
+    echo "***********************************************************************************************"
+    echo "    "
+    echo "    This file is used for SGE job status detction."
+    echo "    "
+    echo "    You can add new SGE job info in this file if you want to add "
+    echo "    new jobs into current test or restart jobs before all"
+    echo "    test jobs are completed"
+    echo "    "
+    echo "    Job info format should looks like as below:"
+    echo "      Your job 534 ("CREW_176x144_30.yuv_SGE_Test_SubCaseIndex_1") has been submitted"
+    echo "    "
+    date
+    echo "*************************************************************************************************"
+    echo ""
+    echo "    All SGE jobs info List As below"
+    echo ""
+    echo "*************************************************************************************************"
+
+}
 runSGEJobSubmit()
 {
-	let "JobNum=0" 
+	let "JobNum=0"
+    runInitialSGEJobInfoFile >${SGEJobListFile}
+
 	for TestYUV in ${aTestYUVList[@]}
 	do
-		SubFolder="${AllTestDataDir}/${TestYUV}"
-		TestSubmitFlagFile="${TestYUV}_Submitted.flag"
-		echo ""
-		echo "test YUV is ${TestYUV}"
-		echo ""
-		
-		if [  -e   ${SubFolder}/${TestSubmitFlagFile} ]
-		then
-			continue
-		fi
-		cd  ${SubFolder}
-		echo "submit job"
-		aSubmitJobList[$JobNum]=`qsub ./${TestYUV}.sge `
-		echo "submit job is ${aSubmitJobList[$JobNum]} "
-		let "JobNum ++" 
-		touch ${TestSubmitFlagFile}		
-		cd  ${CurrentDir}
+        SubFolder="${AllTestDataDir}/${TestYUV}"
+        echo ""
+        echo "test YUV is ${TestYUV}"
+        echo ""
+        echo "******************************************************************************" >>${SGEJobListFile}
+        echo "test YUV is ${TestYUV}" >>${SGEJobListFile}
+        echo "******************************************************************************" >>${SGEJobListFile}
+
+        for vSGEFile in ${SubFolder}/${TestYUV}*.sge
+        do
+            TestSubmitFlagFile="${vSGEFile}_Submitted.flag"
+
+            if [  -e  ${SubFolder}/${TestSubmitFlagFile} ]
+            then
+                continue
+            fi
+
+            echo "submitting job ......"
+            # e.g.: Your job 534 ("CREW_176x144_30.yuv_SGE_Test_SubCaseIndex_1") has been submitted
+            aSubmitJobList[$JobNum]=`qsub  ${vSGEFile} `
+            echo "submit job is ${aSubmitJobList[$JobNum]} "
+            echo "${aSubmitJobList[$JobNum]}" >>${SGEJobListFile}
+            let "JobNum ++"
+            touch ${TestSubmitFlagFile}
+            #cd  ${CurrentDir}
+
+        done
 	done
 	return 0
 }
@@ -316,6 +349,8 @@ runMain()
 	
 	TestFlagFile=""
 	AllTestSummary="${FinalResultDir}/AllTestYUVsSummary.txt"
+    SGEJobListFile="AllSGEJobsInfo.log"
+
 	let "CurrentSGEJobNum=0"
 	declare -a aTestYUVList
 	declare -a aSubmitJobList
