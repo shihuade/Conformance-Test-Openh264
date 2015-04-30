02d1fdef23d15a46f9ccb256915ae66d0a9057c1
huade <huashi@cisco.com>
2015-04-30 14:22:55 +0800

    bug fix for SGE Num

--- Scripts/run_ParseSGEJobIDs.sh
+++ Scripts/run_ParseSGEJobIDs.sh
@@ -19,14 +19,21 @@ runGetAllSGEJobID()
 
 	let "LineIndex=0"
 	let "JobIDIndex=0"
+    let "ExampleLineJobInfoFlag =0"
 	while read line
 	do
         # for line contian the job ID info
         # e.g.: Your job 534 ("CREW_176x144_30.yuv_SGE_Test_SubCaseIndex_1") has been submitted
 		if [[ "$line" =~ "Your job"  ]]
 		then
-			aAllSGEJobIDList[${JobIDIndex}]=`echo $line | awk '{print $3}'`
-			let "JobIDIndex++"
+            # skip the first info which is e.g. info for SGE job
+            if [ ${ExampleLineJobInfoFlag} -eq 0]
+            then
+                let "ExampleLineJobInfoFlag = 1"
+            else
+                aAllSGEJobIDList[${JobIDIndex}]=`echo $line | awk '{print $3}'`
+                let "JobIDIndex++"
+            fi
 		fi
 
         #echo "line is  $line"
--- Scripts/run_ParseSGEJobNames.sh
+++ Scripts/run_ParseSGEJobNames.sh
@@ -19,15 +19,24 @@ runGetAllSGEJobNames()
 
 	let "LineIndex=0"
 	let "JobIDIndex=0"
+    let "ExampleLineJobInfoFlag = 0"
+
 	while read line
 	do
         # for line contian the job ID info
-        # e.g.: Your job 534 ("CREW_176x144_30.yuv_SGE_Test_SubCaseIndex_1") has been submitted
+        # e.g.: Your job 534 ("CREW_176x144_30.yuvCREW_176x144_30.yuv_SGE_Test_SubCaseIndex_1") has been submitted
 		if [[ "$line" =~ "Your job"  ]]
 		then
-			TempName=`echo $line | awk '{print $4}'`
-            aAllSGEJobNameList[${JobIDIndex}]=`echo $TempName | awk 'BEGIN {FS="[()]"} {print $2 }'`
-			let "JobIDIndex++"
+
+            # skip the first info which is e.g. info for SGE job
+            if [ ${ExampleLineJobInfoFlag} -eq 0]
+            then
+                let "ExampleLineJobInfoFlag = 1"
+            else
+                TempName=`echo $line | awk '{print $4}'`
+                aAllSGEJobNameList[${JobIDIndex}]=`echo $TempName | awk 'BEGIN {FS="[()]"} {print $2 }'`
+                let "JobIDIndex++"
+            fi
 		fi
         #echo "line is  $line"
         #echo "LineIndex is ${LineIndex}"
