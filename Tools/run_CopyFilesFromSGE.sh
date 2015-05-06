6ab3c3a67ce0c822288fe90432541dd85a7c930f
huade <huashi@cisco.com>
2015-05-06 15:14:30 +0800

    add tools ----get file full path

--- Tools/run_CopyFilesFromSGE.sh
+++ Tools/run_CopyFilesFromSGE.sh
@@ -1,13 +1,68 @@
 #!/bin/bash
 
 
+runUsage()
+{
+
+}
+
 runInitial()
 {
+    HostName=""
+    JobID=""
+    MasterName=""
+    TestType=""
+
+
 
 
 }
-/opt/sge62u2_1/SGE_room2/OpenH264ConformanceTest/NewSGE-SCC-Test
 
+runCopyFileFormRemoteHost()
+{
+    scp ${HostName}@${HostIP}:${SourceDir}/${FileName}  ${DestDir}
+}
+
+runCheck()
+{
+
+    if [[ $1 =~ "Host-" ]]
+    then
+        HostName=`echo $1 | awk 'BEGIN {FS="-"} {print $2}'`
+        JobID=$2
+        HostIP=``
+
+    elif [[ $1 =~ "Master" ]]
+    then
+
+    else
+        runUsage
+        exit 1
+    fi
+
+
+}
+
+runMain()
+{
+    if  [ $# -lt 3 ]
+    then
+        runUsage
+        exit 1
+    fi
+
+    runInitial
+    ConfigureFile=$1
+
+    runCheck $1 $2
+    runGetSourceIP
+    runGetSourceDir
+
+    runCopyFile
+
+
+
+}
 
 
 
