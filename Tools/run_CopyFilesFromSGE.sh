c3ec36b75409dd268286d3e7fb2d14f5e34a1e19
huade <huashi@cisco.com>
2015-05-06 17:12:57 +0800

    basic framework for copy SGE files from SGE

--- Tools/run_CopyFilesFromSGE.sh
+++ Tools/run_CopyFilesFromSGE.sh
@@ -3,68 +3,149 @@
 
 runUsage()
 {
-
+    echo ""
+    echo -e "\033[31m Usage: run_CopyFilesFromSGE.sh  \${SGEConfigureFile}   \033[0m"
+    echo ""
+    echo -e "\033[32m  e.g.: run_CopyFilesFromSGE.sh  SGE.cfg                \033[0m"
+    echo ""
 }
+
 
 runInitial()
 {
     HostName=""
     JobID=""
-    MasterName=""
     TestType=""
 
-
-
+    RemoteIP=""
+    RemoteSourceDir=""
+    RemoteSourceFilePath=""
+    RemoteUser="root"
+    ConfigureFile="SGE.cfg"
+    CurrentDir=`pwd`
+    TempDestDir="TempTestData"
+    HostDataFolder="result"
+    MasterFolder="FinalResult"
 
 }
 
 runCopyFileFormRemoteHost()
 {
-    scp ${HostName}@${HostIP}:${SourceDir}/${FileName}  ${DestDir}
+    echo "scp ${RemoteUser}@${RemoteIP}:${RemoteSourceFilePath}  ${DestDir}"
+    scp ${RemoteUser}@${RemoteIP}:${RemoteSourceFilePath}  ${DestDir}
+}
+
+runRemoteInfoCheck()
+{
+    if [ "${RemoteIP}" = "" ]
+    then
+        echo ""
+        echo -e "\033[31m Remote IP can not find, please configure in the SGE.cfg file!   \033[0m"
+        echo -e "\033[31m  host name is ${HostName}   \033[0m"
+        echo ""
+        runUsage
+        exit 1
+    elif [[ "${RemoteIP}" =~ " " ]]
+    then
+
+        echo ""
+        echo -e "\033[31m  Multiple IP of Remote host is not support, please use single remote host!   \033[0m"
+        echo -e "\033[31m  RemoteIP is ${RemoteIP}   \033[0m"
+        echo ""
+        runUsage
+        exit 1
+    fi
+
+    if [ "${RemoteSourceDir}" = "" ]
+    then
+        echo ""
+        echo -e "\033[31m Remote host source dir can not find, please configure in the SGE.cfg file!   \033[0m"
+        echo -e "\033[31m  host name is ${HostName}   \033[0m"
+        echo ""
+        runUsage
+        exit 1
+    elif [[ "${RemoteSourceDir}" =~ " " ]]
+    then
+        echo ""
+        echo -e "\033[31m  Multiple RemoteSourceDir of Remote host is not support, please use single remote host!   \033[0m"
+        echo -e "\033[31m  RemoteSourceDir is ${RemoteSourceDir}   \033[0m"
+        echo ""
+        runUsage
+        exit 1
+    fi
+}
+runParseInputOption()
+{
+
+    if [[ "${HostOption}" =~ "Host-" ]]
+    then
+        HostName=`echo $HostOption | awk 'BEGIN {FS="-"} {print $2}'`
+        JobID=${HostType}
+        RemoteIP=`./run_ParseSGEHostsIP.sh  ${ConfigureFile}  ${HostOption} `
+        RemoteSourceDir=`./run_ParseSGEHostsTestDataDir.sh  ${ConfigureFile}  ${HostOption} `
+        RemoteSourceFilePath="${RemoteSourceDir}/SGEJobID_${JobID}/${HostDataFolder}/${FileName}"
+
+    elif [[ "${HostOption}" =~ "Master" ]]
+    then
+        HostName="Master-LiuBei"
+        TestType=${HostType}
+        RemoteIP=`./run_ParseSGEHostsIP.sh  ${ConfigureFile}  Master `
+        #Dir-Master-LiuBei-SCC
+        RemoteSourceDir=`./run_ParseSGEHostsTestDataDir.sh   ${ConfigureFile}  "Master-${TestType}" `
+        RemoteSourceFilePath="${RemoteSourceDir}/${MasterFolder}/${FileName}"
+    else
+        echo -e "\033[31m option error!  \033[0m"
+        runUsage
+        exit 1
+    fi
+
 }
 
 runCheck()
 {
-
-    if [[ $1 =~ "Host-" ]]
+    if [ ! -e ${ConfigureFile}  ]
     then
-        HostName=`echo $1 | awk 'BEGIN {FS="-"} {print $2}'`
-        JobID=$2
-        HostIP=``
-
-    elif [[ $1 =~ "Master" ]]
-    then
-
-    else
-        runUsage
+        echo ""
+        echo -e "\033[31m ConfigureFile ${ConfigureFile} does not exist, please double check!   \033[0m"
+        echo ""
         exit 1
     fi
 
-
+    if [ "${DestDir}" = "" ]
+    then
+        DestDir=${CurrentDir}
+    elif [ -d "${DestDir}"  ]
+    then
+        cd ${DestDir}
+        DestDir=`pwd`
+        cd ${CurrentDir}
+    else
+        echo ""
+        echo -e "\033[31m DestDir ${DestDir} does not exist, please double check!   \033[0m"
+        echo ""
+        exit 1
+    fi
 }
 
 runMain()
 {
-    if  [ $# -lt 3 ]
-    then
-        runUsage
-        exit 1
-    fi
-
     runInitial
-    ConfigureFile=$1
-
-    runCheck $1 $2
-    runGetSourceIP
-    runGetSourceDir
-
-    runCopyFile
-
-
+    runCheck
+    runParseInputOption
+    runRemoteInfoCheck
+    runCopyFileFormRemoteHost
 
 }
 
+HostOption=$1
+HostType=$2
+FileName=$3
+DestDir=$4
+echo "FileName is ${FileName}"
+if  [ $# -lt 3 ]
+then
+    runUsage
+    exit 1
+fi
 
-
-
-
+runMain
