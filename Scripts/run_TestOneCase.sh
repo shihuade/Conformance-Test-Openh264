b6bc700f4ea3ef35bcea42bd95e12ed35385db2f
Forrest Shi <huashi@cisco.com>
2015-12-03 14:19:31 +0800

    Update run_TestOneCase.sh

--- Scripts/run_TestOneCase.sh
+++ Scripts/run_TestOneCase.sh
@@ -53,7 +53,7 @@ runEncoderCommandInital()
 					"-slcmd 0" "-slcnum 0" "-slcmd 1" "-slcnum 1"\
 					"-slcmd 2" "-slcnum 2" "-slcmd 3" "-slcnum 3"\
 					-nalsize \
-					-iper   -thread  " -loadbalancing 0 "  -ltr \
+					-iper   -thread  " -loadbalancing "  -ltr \
 					-db  -denois    -scene    -bgd    -aq )
 	aEncoderCommandName=(usagetype  frms  numl   numtl \
 					sw sh  dw0 dh0 dw1 dh1 dw2 dh2 dw3 dh3 \
